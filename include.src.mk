c4a5e2da0280e7b43979c2ea68ac759cda208b3a
Rich <rich@tongfamily.com>
2020-09-25 16:35:47 -0700

    add restart_datasets

--- include.src.mk
+++ include.src.mk
@@ -1,6 +1,6 @@
 PYTHON=3.8.5
 PACKAGES+=make vim gosu
-PIP+=pandas confuse ipysheet pyomo h5py
+PIP+=pandas confuse ipysheet pyomo h5py restart_datasets
 		   # pyyaml xlrd
 PIP_ONLY+=tables
 # These are for development time

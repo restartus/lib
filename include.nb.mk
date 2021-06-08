99c01e5bb73f7a67d7fba1d55558cb4784828b56
Rich Tong <1782087+richtong@users.noreply.github.com>
2021-06-08 11:44:40 -0700

    make doc to take notebook to docx but does not hide cells

--- include.nb.mk
+++ include.nb.mk
@@ -7,3 +7,11 @@ PACKAGES+=make vim gosu
 PIP+=pandas confuse ipysheet h5py ipywidgets ipympl ipyvuetify \
      scipy altair xlrd bqplot ipyvolume restart==2.6.7 restart_datasets voila
 PIP_ONLY+=tables qgrid
+
+NOTEBOOK ?= notebook.ipynb
+
+
+## docx: Convert from .ipynb to .docx
+.phony: docx
+docx:
+	pandoc $(NOTEBOOK) -s -o "$$(basename -s ipynb "$(NOTEBOOK)")docx"
